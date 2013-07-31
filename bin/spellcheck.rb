8b04dcc556edd3359345fae6093ff0c2e6a5d67c
Rick <rick@cloudscaling.com>
2013-07-31 01:06:36 -0700

    polishing
    fix prompt
    expand the code covered by rescue Interrupt (more or less originally)
    make sure we can pipe no matter the dict situation

--- bin/spellcheck.rb
+++ bin/spellcheck.rb
@@ -1,39 +1,42 @@
 #!/usr/bin/env ruby
 
-$stdout.sync = true
-
 default_dict = '/usr/share/dict/words'
-DICT = File.exists?(default_dict) ? default_dict : nil
+DICT = File.exists?(default_dict) && default_dict
 
 unless DICT
-  $stderr.puts "Note: could not find default dict #{default_dict}"
-  $stderr.puts "Try installing `wamerican` on Debian based systems"
-  $stderr.puts
+  warn "Note: could not find default dict #{default_dict}"
+  warn "Try installing `wamerican` on Debian based systems"
 end
 
 def prompt_dict
   msg = "Dictionary path required:"
   msg << " (#{DICT})" if DICT
-  print "#{msg}\n> "
-  $stdin.gets.chomp
+  $stderr.print "#{msg}\n> "
+  dict = $stdin.gets.chomp
+  if dict.empty?
+    return DICT if DICT
+    warn "Dictionary path required"
+    exit 1
+  end
+  dict
 end
 
 
 # embed a tiny, completely different program for generating words
 #
 if ARGV[0].to_s.downcase == 'generate'
+  ARGV.shift
+  $stdout.sync = true
+
   require 'spellbreaker'
 
-  ARGV.shift
-  dict = ARGV.shift || prompt_dict
-  spell = Misspell.new
-  spell.build((dict.empty?) ? dict = '/usr/share/dict/words' : dict)
-  puts dict # Feed dictionary path to spellchecker
-
   begin
-    loop do
-      puts spell.wrong(spell.dictionary.sample)
-    end
+    dict = ARGV.shift || prompt_dict
+    spell = Misspell.new
+    spell.build dict
+    puts dict # Feed dictionary path to spellchecker
+
+    loop { puts spell.wrong(spell.dictionary.sample) }
   rescue Interrupt
     warn "\nBye!"
     exit
@@ -44,25 +47,24 @@ end
 #
 require 'spellcheck'
 
-dict = ARGV.shift || prompt_dict
-
-# Make a trie
-trieHard = Trie.new
-# Make a state handler to hold each step's calculations
-stateHandler = State.new
-# If they didn't pass a path, use the unix default, otherwise use theirs
-trieHard.build((dict.empty?) ? dict = '/usr/share/dict/words' : dict)
-# Makin' Bacon
-print "Loaded, ready!\n"
-
-# Now we just spin our wheels waiting on user input
-# working when needed
 begin
+  dict = ARGV.shift || prompt_dict
+
+  # Make a trie
+  trieHard = Trie.new
+  stateHandler = State.new
+
+  # Makin' Bacon
+  trieHard.build dict
+  puts "Loaded, ready!"
+
+  # Now we just spin our wheels waiting on user input
+  # working when needed
   loop do
     # User prompt
     print "> "
-    input = $stdin.gets.chomp.strip
-    if !input.empty?
+    input = $stdin.gets.chomp
+    unless input.empty?
       # An edit distance of your maximum string length seems to be a
       # good upper limit
       puts trieHard.step(input, trieHard.maxLength, stateHandler)
